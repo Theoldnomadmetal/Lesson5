00704b33ee620639d3f7a793c0a8cc457aa46f83
Theoldnomadmetal <Deadharmonic@gmail.com>
2020-07-08 15:21:48 +0600

    Lesson5

--- app/src/main/java/com/example/lesson5/MainActivity.kt
+++ app/src/main/java/com/example/lesson5/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.lesson5
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var loginMain:EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginmain = findViewById<EditText>(R.id.etlogin)
+        val passwordmain = findViewById<EditText>(R.id.etpassword)
+        val btnEnter = findViewById<Button>(R.id.button_login)
+        val btnRegister = findViewById<Button>(R.id.button_reg)
+
+        val profilePreference = getSharedPreferences("profPref", Context.MODE_PRIVATE)
+        val passwordPreference = getSharedPreferences("passPref",Context.MODE_PRIVATE)
+
+
+
+        btnRegister.setOnClickListener {
+            val profText = loginmain.text.toString()
+            val passProfText = passwordmain.text.toString()
+
+            profilePreference.edit().putString("profPref",profText).apply()
+            passwordPreference.edit().putString("passPref",passProfText).apply()
+
+            Toast.makeText(applicationContext,"Вы зарегистрированы",Toast.LENGTH_LONG).show()
+        }
+        btnEnter.setOnClickListener {
+            val compareProfile = loginmain.text.toString()
+            val comparePassword = passwordmain.text.toString()
+
+            val passFromPreference = passwordPreference.getString("passPref","Нет данных")
+            val textfromPreference = profilePreference.getString("profPref", "Нет данных")
+            if (compareProfile == textfromPreference && comparePassword == passFromPreference){
+                val intent = Intent(this, SecondActivity::class.java)
+
+
+                startActivity(intent)
+
+            }else Toast.makeText(applicationContext,"Данные не найдены",Toast.LENGTH_LONG).show()
+
+
+
+
+
+
+
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+
     }
 }
--- app/src/main/java/com/example/lesson5/SecondActivity.kt
+++ app/src/main/java/com/example/lesson5/SecondActivity.kt
@@ -1,11 +1,122 @@
 package com.example.lesson5
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val loginProfile = findViewById<EditText>(R.id.etprofile_login)
+        val loginPassword = findViewById<EditText>(R.id.etprofile_password)
+        val buttonRefresh = findViewById<Button>(R.id.button_refresh)
+        val buttonExit = findViewById<Button>(R.id.button_exit)
+
+
+        val loginText = loginProfile.text.toString()
+        val passwordText = loginPassword.text.toString()
+
+        val profilePreference = getSharedPreferences("profPref", Context.MODE_PRIVATE)
+        val passwordPreference = getSharedPreferences("passPref",Context.MODE_PRIVATE)
+
+        buttonRefresh.setOnClickListener {
+            profilePreference.edit().putString("profPref",loginText).apply()
+            passwordPreference.edit().putString("passPref", passwordText).apply()
+
+            Toast.makeText(applicationContext,"Данные обновлены",Toast.LENGTH_LONG).show()
+
+        }
+        buttonExit.setOnClickListener {
+            profilePreference.edit().putString("profPref",loginText).clear()
+            passwordPreference.edit().putString("passPref", passwordText).clear()
+            Toast.makeText(applicationContext,"Данные очищены",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
